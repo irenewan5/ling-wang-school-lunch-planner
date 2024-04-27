a52ff4cc14c7610e545af234613b8e7ffe7bc111
Ling Wang <buthowever@gmail.com>
2024-04-27 20:45:42 -0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import "./App.scss";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import PlansPage from "./pages/PlansPage/PlansPage";
 import SearchPage from "./pages/SearchPage/SearchPage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RecipePage from "./pages/RecipePage/RecipePage";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Navigate to="/plans" />} />
         <Route path="/plans" element={<PlansPage />} />
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom";
+import "./Navbar.scss";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar__logo">School Lunch Planner</div>
+      <ul className="navbar__menu">
+        <li className="navbar__menu-item">
+          <NavLink to="/plans">Plans</NavLink>
+        </li>
+        <li className="navbar__menu-item">
+          <NavLink to="/profile">Profile</NavLink>
+        </li>
+        <li className="navbar__menu-item">
+          <NavLink to="/logout">Log out</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
