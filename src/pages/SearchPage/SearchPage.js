b8230e415297d10b9504bf6f3fe3130e51a604f9
Ling Wang <buthowever@gmail.com>
2024-05-03 14:30:56 -0300

    add search page with sample data

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,5 +1,38 @@
+import axios from "axios";
+import { useState } from "react";
+import recipesData from "../../assets/data/recipes.json";
+
 function SearchPage() {
-  return <>SearchPage</>;
+  const [q, setQ] = useState("");
+  const [recipes, setRecipes] = useState(recipesData);
+
+  const search = async () => {
+    const resp = await axios.get("http://localhost:3001/recipes", {
+      params: {
+        q,
+      },
+    });
+    setRecipes(resp.data);
+  };
+
+  return (
+    <>
+      <div>
+        <input value={q} onChange={(evt) => setQ(evt.target.value)} />
+        <button onClick={search}>search</button>
+      </div>
+      <div>
+        {recipes.items?.map((item) => (
+          <div key={item.id}>
+            <div>{item.label}</div>
+            <div>
+              <img src={item.image} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default SearchPage;
