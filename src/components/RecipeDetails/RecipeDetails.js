9eac2c0bd8ad07dbcf34f96e2602a7fbae95b95d
Ling Wang <buthowever@gmail.com>
2024-05-06 18:23:24 -0300

    add responsive styling for recipe details page

--- src/components/RecipeDetails/RecipeDetails.js
+++ src/components/RecipeDetails/RecipeDetails.js
@@ -5,18 +5,20 @@ function RecipeDetails({ recipe }) {
   return (
     <div className="recipedetails">
       <h2 className="recipedetails__name">{recipe.label}</h2>
-      <img className="recipedetails__image" src={recipe.image} />
-      <div className="recipedetails__ingredients">
-        <h3 className="recipedetails__subtitle">Ingredients</h3>
-        <div className="recipedetails__ingredients-list">
-          {recipe.ingredients.map((ingredient) => (
-            <div
-              key={ingredient.foodId}
-              className="recipedetails__ingredients-item"
-            >
-              <div>{ingredient.text}</div>
-            </div>
-          ))}
+      <div className="recipedetails__info">
+        <img className="recipedetails__image" src={recipe.image} />
+        <div className="recipedetails__ingredients">
+          <h3 className="recipedetails__subtitle">Ingredients</h3>
+          <div className="recipedetails__ingredients-list">
+            {recipe.ingredients.map((ingredient) => (
+              <div
+                key={ingredient.foodId}
+                className="recipedetails__ingredients-item"
+              >
+                <div>{ingredient.text}</div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <div>
